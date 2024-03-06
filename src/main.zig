9e9c806a906fb9cf57e7a3f691f2cfe0ab0f504e
Casey Clayton <claytoncasey01@gmail.com>
2024-03-06 00:27:20 -0500

    Use zig-clap for cli args

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const args = @import("args.zig");
+const clap = @import("deps/zig-clap/clap.zig");
 const FoundItem = @import("types.zig").FoundItem;
 const fileHandler = @import("file.zig");
 const colorizeWord = @import("output.zig").colorizeWord;
@@ -8,28 +8,58 @@ const OutputOptions = @import("output.zig").OutputOptions;
 const writeOutput = @import("output.zig").writeOutput;
 
 pub fn main() !void {
-    // NOTE: std.os.args does not work on windows or wasi
-    const cli_args = std.os.argv;
-    const parsed_args = args.parseArgs(cli_args);
     var gpa = std.heap.GeneralPurposeAllocator(.{ .verbose_log = true }){};
-    // defer std.debug.assert(gpa.deinit() == .ok);
+    defer _ = gpa.deinit();
+
+    // Setup command line arguments
+    const params = comptime clap.parseParamsComptime(
+        \\-h, --help Display this help and exit.
+        \\-n, --line-number Prefix each line of output with the 1-based line number within its input file.
+        \\-H, --with-filename Print the file name for each match.
+        \\-i, --ignore-case Ignore case distinctions in both the PATTERN and the input files.
+        \\-v, --invert-match Invert the sense of matching, to select non-matching lines.
+        \\-w, --word-regexp Select only those lines containing matches that form whole words.
+        \\-c, --color <str>... Select which color to be used when displaying the match. Defaults to green. 
+        \\<str> The text to search for
+        \\<str> The file or text to search in
+    );
+
     const allocator = gpa.allocator();
+
+    // Parse line arguments
+    var diag = clap.Diagnostic{};
+    const parsed_args = clap.parse(clap.Help, &params, clap.parsers.default, .{ .diagnostic = &diag, .allocator = allocator }) catch |err| {
+        diag.report(std.io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer parsed_args.deinit();
 
     var found = std.ArrayList(FoundItem).init(allocator);
     defer found.deinit();
 
-    // In this case we most likely are dealing with a file or directory, just assume so for now
-    if (std.fs.cwd().statFile(parsed_args.text)) |stat| {
-        switch (stat.kind) {
-            .directory => std.debug.print("{s} is a directory\n", .{parsed_args.text}),
-            .file => try fileHandler.searchFile(parsed_args.text, parsed_args.search_for, &found),
-            else => std.debug.print("{s} is not a file or directory\n", .{parsed_args.text}),
+    if (parsed_args.positionals.len == 2) {
+        // Pull out our positional arguments
+        const search_for = parsed_args.positionals[0];
+        const search_in = parsed_args.positionals[1];
+
+        // In this case we most likely are dealing with a file or directory, just assume so for now
+        if (std.fs.cwd().statFile(search_in)) |stat| {
+            switch (stat.kind) {
+                .directory => std.debug.print("{s} is a directory\n", .{search_in}),
+                .file => try fileHandler.searchFile(search_in, search_for, &found),
+                else => std.debug.print("{s} is not a file or directory\n", .{search_in}),
+            }
+        } else |err| switch (err) {
+            error.FileNotFound => try search(search_in, search_for, &found),
+            else => std.debug.print("An error occured", .{}),
         }
-    } else |err| switch (err) {
-        error.FileNotFound => try search(parsed_args.text, parsed_args.search_for, &found),
-        else => std.debug.print("An error occured", .{}),
+        try writeOutput(&found, search_for, OutputOptions{ .line_number = false, .file_path = null });
+    } else if (parsed_args.args.help != 0) {
+        try clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
+    } else {
+        try clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
+        try diag.report(std.io.getStdErr().writer(), error.NotEnoughArguments);
     }
-    try writeOutput(&found, parsed_args.search_for, OutputOptions{ .line_number = false, .file_path = null });
 }
 
 // Takes a string to be searched and a string to search for. It will return
