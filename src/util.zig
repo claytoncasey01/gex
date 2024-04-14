354493696a5da3d8bbc2fcbbd7b62345067453e4
Casey Clayton <claytoncasey01@gmail.com>
2024-04-14 18:57:46 -0400

    Enhancement/benchmarks (#11)
    * Add benchmarks for colorizeWord
    
    * Add zBench dep
    
    * Add benchmark for toCString
    
    * Add unit test for toCString

--- src/util.zig
+++ src/util.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Benchmark = @import("deps/zBench//zbench.zig").Benchmark;
 
 // TODO: This can be updated to use std.mem.span and then allocator.dupeZ for better performance
 pub fn toCString(input: []const u8, allocator: std.mem.Allocator) ![:0]const u8 {
@@ -7,6 +8,28 @@ pub fn toCString(input: []const u8, allocator: std.mem.Allocator) ![:0]const u8 
     std.mem.copyForwards(u8, buffer, input);
     buffer[buffer_len - 1] = 0;
     return buffer[0 .. buffer.len - 1 :0];
+}
+
+fn benchmarkToCString(allocator: std.mem.Allocator) void {
+    const input = "Hello, World!";
+    const c_string = toCString(input, allocator) catch unreachable;
+    allocator.free(c_string);
+}
+
+test "toCString" {
+    const allocator = std.testing.allocator;
+    const input = "Hello, World!";
+    const c_string = try toCString(input, allocator);
+    defer allocator.free(c_string);
+    try std.testing.expectEqualStrings(input, c_string);
+}
+
+test "Benchmark toCString" {
+    const allocator = std.testing.allocator;
+    var bench = Benchmark.init(allocator, .{});
+    defer bench.deinit();
+    try bench.add("toCString", benchmarkToCString, .{});
+    try bench.run(std.io.getStdOut().writer());
 }
 
 // TODO: Unused for now but will be udpated with an optimization pass
